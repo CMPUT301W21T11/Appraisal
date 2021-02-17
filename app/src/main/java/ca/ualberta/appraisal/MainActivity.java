27d86e46c898c1ee9999626218f4b195a1ac888b
Danh Nguyen <huudanh2908@gmail.com>
2021-02-17 17:41:39 -0500

    Temp ini need everyone test by open it in Android Studio

---
+++ app/src/main/java/ca/ualberta/appraisal/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.ualberta.appraisal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
