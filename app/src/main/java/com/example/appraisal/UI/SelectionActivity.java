bc2936f7198ff0aaa6b694351d1287fbed225188
Danh Nguyen <huudanh2908@gmail.com>
2021-02-27 16:21:38 -0500

    Add Main Menu

--- app/src/main/java/com/example/appraisal/UI/SelectionActivity.java
+++ app/src/main/java/com/example/appraisal/UI/SelectionActivity.java
@@ -1,12 +1,20 @@
 package com.example.appraisal.UI;
 
+import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.appraisal.R;
 
 public class SelectionActivity extends AppCompatActivity {
     // TODO
     // Needs 4 button action for the user to choose the experiment type, then bring the user
     // to the corresponding activity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection);
+    }
 
     public void toBinomial(View v) {
         // TODO
