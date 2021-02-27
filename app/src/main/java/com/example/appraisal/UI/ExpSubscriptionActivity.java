eced7e884ed64de4df1ba5ab9223efa842ee65a3
Danh Nguyen <huudanh2908@gmail.com>
2021-02-27 17:55:53 -0500

    Menubar ready

---
+++ app/src/main/java/com/example/appraisal/UI/ExpSubscriptionActivity.java
@@ -0,0 +1,37 @@
+package com.example.appraisal.UI;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.appraisal.R;
+
+public class ExpSubscriptionActivity extends AppCompatActivity {
+
+    // TODO
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscription);
+    }
+
+    public void toHome(View v) {
+        // TODO When click on home button
+    }
+
+    public void toSearch(View v) {
+        // TODO When click on search button
+    }
+
+    public void toForum(View v) {
+        // TODO When click on forum button
+    }
+
+    public void toMyExps(View v) {
+        // TODO When click on expList button
+    }
+
+    public void toProfile(View v) {
+        // TODO When click on profile button
+    }
+}
