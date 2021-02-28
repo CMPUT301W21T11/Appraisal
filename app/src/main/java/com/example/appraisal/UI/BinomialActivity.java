cb6074a417333db2bbfed75659f385941b0ce284
Urvi Patel <upatel@ualberta.ca>
2021-02-28 11:50:41 -0700

    changed binomial classes to include backend and model

--- app/src/main/java/com/example/appraisal/UI/BinomialActivity.java
+++ app/src/main/java/com/example/appraisal/UI/BinomialActivity.java
@@ -10,64 +10,40 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.appraisal.R;
-
+import com.example.appraisal.model.BinomialModel;
 
 
 public class BinomialActivity extends AppCompatActivity {
-    int success_counter = 0;
-    int failure_counter = 0;
-
-    Button success_btn;
-    Button failure_btn;
-
-    TextView success_txt;
-    TextView failure_txt;
+    private TextView success_txt;
+    private TextView failure_txt;
+    private BinomialModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_binomial);
-        success_btn_func();
-        failure_btn_func();
 
-    }
-
-    public void increaseType1Result(View v) {
-        // TODO
-    }
-
-    public void increaseType2Result(View v) {
-        // TODO
-    }
-
-    public void submit(View v) {
-        // TODO
-    }
-
-
-    public void failure_btn_func() {
-        failure_btn = findViewById(R.id.failure_button);
-        failure_txt = findViewById(R.id.failure_count);
-
-        failure_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                failure_counter = failure_counter + 1;
-                failure_txt.setText(Integer.toString(failure_counter));
-            }
-        });
-    }
-
-    public void success_btn_func() {
-        success_btn = findViewById(R.id.success_button);
         success_txt = findViewById(R.id.success_count);
-
-        success_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                success_counter = success_counter + 1;
-                success_txt.setText(Integer.toString(success_counter));
-            }
-        });
+        failure_txt = findViewById(R.id.failure_count);
+        model = new BinomialModel();
     }
+
+    public void incrementSuccess(View v){
+        // adjust model
+        model.addSuccess();
+
+        // get new data from model, and set text
+        String count = String.valueOf(model.getSuccessCount());
+        success_txt.setText(count);
+    }
+
+    public void incrementFailure(View v){
+        //adjust model
+        model.addFailure();
+
+        // get new data from model, and set text
+        String count = String.valueOf(model.getFailureCount());
+        failure_txt.setText(count);
+    }
+
 }
