c0d40042358c878a960183febe30f05819a56707
Danh Nguyen <huudanh2908@gmail.com>
2021-02-27 22:31:19 -0500

    Add temporary ListView

--- app/src/main/java/com/example/appraisal/UI/ExpSubscriptionActivity.java
+++ app/src/main/java/com/example/appraisal/UI/ExpSubscriptionActivity.java
@@ -2,17 +2,34 @@ package com.example.appraisal.UI;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.appraisal.R;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ExpSubscriptionActivity extends AppCompatActivity {
 
     // TODO
+    private ListView subscribed_list;
+    private List<String> subscribed_content;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscription);
+
+        // Testing dummy data
+        String[] dummy = {"a", "b", "c"};
+        subscribed_content = Arrays.stream(dummy).collect(Collectors.toList());
+
+        subscribed_list = findViewById(R.id.subscribedList);
+        adapter = new ArrayAdapter<>(this, R.layout.list_content, subscribed_content);
+        subscribed_list.setAdapter(adapter);
     }
 
     public void toHome(View v) {
