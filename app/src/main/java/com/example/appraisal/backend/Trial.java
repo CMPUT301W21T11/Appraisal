7406ff11827617145aa7ca9a3c38433bf7f754d3
flyrobot27 <stevenheung.ca@gmail.com>
2021-03-02 20:14:52 -0400

    implement Trail abstract class

--- app/src/main/java/com/example/appraisal/backend/Trial.java
+++ app/src/main/java/com/example/appraisal/backend/Trial.java
@@ -1,4 +1,30 @@
 package com.example.appraisal.backend;
 
 abstract class Trial {
+    protected final User conductor;
+    protected final GeoLocation location;
+    protected final String parentExperimentID;
+
+    /** Constructor for non-geolocation trials */
+    public Trial(User conductor, String parentExperimentID) {
+        this.conductor = conductor;
+        this.parentExperimentID = parentExperimentID;
+        location = null;
+    }
+
+    /** Constructor for geolocation trials */
+    public Trial(User conductor, String parentExperimentID, GeoLocation location) {
+        this.conductor = conductor;
+        this.parentExperimentID = parentExperimentID;
+        this.location = location;
+    }
+
+    public String getParentExperimentID() {
+        return parentExperimentID;
+    }
+
+    public GeoLocation getGeoLocation() {
+        return location;
+    }
+
 }
