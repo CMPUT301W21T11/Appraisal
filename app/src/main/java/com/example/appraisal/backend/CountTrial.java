c9922e4f9cfb6fa452b33e65c80ac7a53d9e4f49
flyrobot27 <stevenheung.ca@gmail.com>
2021-03-05 22:01:18 -0400

    app fixed

--- app/src/main/java/com/example/appraisal/backend/CountTrial.java
+++ app/src/main/java/com/example/appraisal/backend/CountTrial.java
@@ -3,21 +3,4 @@ package com.example.appraisal.backend;
 import android.os.Build;
 
 public class CountTrial extends Trial {
-    private int counter;
-
-    public CountTrial(User conductor, GeoLocation location) {
-        super(conductor, location);
-        counter = 0;
-    }
-    public void increase() {
-        counter++;
-    }
-
-    public void setCounter(int counter) {
-        this.counter = counter;
-    }
-
-    public int getCount() {
-        return counter;
-    }
 }
