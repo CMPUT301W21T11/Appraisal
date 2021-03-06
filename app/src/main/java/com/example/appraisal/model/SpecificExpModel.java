70daf53affc4b21a7fef7f535fb0c7c57ae3271d
flyrobot27 <stevenheung.ca@gmail.com>
2021-03-06 01:21:08 -0400

    Specific Experiment Activity UI complete (Other than QR codes)

--- app/src/main/java/com/example/appraisal/model/SpecificExpModel.java
+++ app/src/main/java/com/example/appraisal/model/SpecificExpModel.java
@@ -1,4 +1,7 @@
 package com.example.appraisal.model;
 
 public class SpecificExpModel {
+    // This model is currently empty because other class such as User and Experiment is not finalized yet
+    // SpecificExpActivity is currently running of dummy code and will require a refactor
+    // once those classes are implemented
 }
