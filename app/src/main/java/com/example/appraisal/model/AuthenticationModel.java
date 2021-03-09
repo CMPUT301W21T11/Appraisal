411aa792f188473f7b43a5af058a82647db97eb3
Restyled.io <commits@restyled.io>
2021-03-09 04:30:09 +0000

    Restyled by clang-format

--- app/src/main/java/com/example/appraisal/model/AuthenticationModel.java
+++ app/src/main/java/com/example/appraisal/model/AuthenticationModel.java
@@ -3,34 +3,28 @@ package com.example.appraisal.model;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class AuthenticationModel {
-    // Authentication
-    protected FirebaseAuth mAuth;
+  // Authentication
+  protected FirebaseAuth mAuth;
 
-    public AuthenticationModel(){
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
-    }
+  public AuthenticationModel() {
+    // Initialize Firebase Auth
+    mAuth = FirebaseAuth.getInstance();
+  }
 
-    /**
-     * Sign in the user anonymously
-     */
-    public void sign_in() {
-        mAuth.signInAnonymously();
-    }
+  /**
+   * Sign in the user anonymously
+   */
+  public void sign_in() { mAuth.signInAnonymously(); }
 
-    /**
-     * Used to sign out a user but will not be available to the user.
-     */
-    public void sign_out(){
-        mAuth.signOut();
-    }
+  /**
+   * Used to sign out a user but will not be available to the user.
+   */
+  public void sign_out() { mAuth.signOut(); }
 
-    /**
-     * Get the user id
-     * @return
-     */
+  /**
+   * Get the user id
+   * @return
+   */
 
-    public String get_userID(){
-        return mAuth.getCurrentUser().getUid();
-    }
+  public String get_userID() { return mAuth.getCurrentUser().getUid(); }
 }
