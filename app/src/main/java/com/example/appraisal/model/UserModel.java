4664c1734144ff7a732a8bd583e6906c4bbbbe05
Justin Monteza <26825560+jmonteza@users.noreply.github.com>
2021-03-08 20:34:43 -0700

    Firebase authentication done

---
+++ app/src/main/java/com/example/appraisal/model/UserModel.java
@@ -0,0 +1,38 @@
+package com.example.appraisal.model;
+
+public class UserModel {
+    String userID;
+    String email;
+    String phone;
+
+    public UserModel(String userID, String email, String phone){
+        this.userID = userID;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
