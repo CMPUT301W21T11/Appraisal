530db589517cf0bb6ff4a3ff9832d583edf2fd81
Restyled.io <commits@restyled.io>
2021-03-09 21:46:30 +0000

    Restyled by clang-format

--- app/src/main/java/com/example/appraisal/UI/MainActivity.java
+++ app/src/main/java/com/example/appraisal/UI/MainActivity.java
@@ -3,29 +3,26 @@ package com.example.appraisal.UI;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.appraisal.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
 
-    public void toSelection(View v) {
-        // TODO Bring the user from main menu, to experiment type menu
-        Intent intent = new Intent(this, ExpSubscriptionActivity.class);
-        startActivity(intent);
-    }
+  public void toSelection(View v) {
+    // TODO Bring the user from main menu, to experiment type menu
+    Intent intent = new Intent(this, ExpSubscriptionActivity.class);
+    startActivity(intent);
+  }
 
-    public void toCheckUsername(View v) {
-        // TODO Bring the user from main menu, to experiment type menu
-        Intent intent = new Intent(this, AuthenticationActivity.class);
-        startActivity(intent);
-    }
-
+  public void toCheckUsername(View v) {
+    // TODO Bring the user from main menu, to experiment type menu
+    Intent intent = new Intent(this, AuthenticationActivity.class);
+    startActivity(intent);
+  }
 }
