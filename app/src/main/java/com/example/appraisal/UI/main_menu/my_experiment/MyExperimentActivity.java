f8a797400e51c45c1183f55bf43db11f1fdd525f
Danh Nguyen <huudanh2908@gmail.com>
2021-03-10 23:40:46 -0500

    Soft cp Experiment implemented. MainModel global getter, setter.

--- app/src/main/java/com/example/appraisal/UI/main_menu/my_experiment/MyExperimentActivity.java
+++ app/src/main/java/com/example/appraisal/UI/main_menu/my_experiment/MyExperimentActivity.java
@@ -1,10 +1,15 @@
 package com.example.appraisal.UI.main_menu.my_experiment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import com.example.appraisal.R;
 import com.example.appraisal.UI.main_menu.MainMenuCommonActivity;
+import com.example.appraisal.UI.main_menu.SelectionActivity;
+import com.example.appraisal.backend.experiment.Experiment;
+import com.example.appraisal.backend.user.User;
 import com.example.appraisal.model.MainModel;
 import com.example.appraisal.model.MyExperimentModel;
 
@@ -28,5 +33,11 @@ public class MyExperimentActivity extends MainMenuCommonActivity {
         
     }
 
+    public void toNewExperiment(View view) throws Exception {
+        Intent intent = new Intent(this, SelectionActivity.class);
 
+        // TODO to be refactored
+        Experiment dummy_experiment = new Experiment("Dummy title", "Dummy description",
+               MainModel.getCurrentUser());
+    }
 }
--- app/src/main/java/com/example/appraisal/model/MainModel.java
+++ app/src/main/java/com/example/appraisal/model/MainModel.java
@@ -1,5 +1,7 @@
 package com.example.appraisal.model;
 
+import com.example.appraisal.backend.experiment.Experiment;
+import com.example.appraisal.backend.user.User;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -16,17 +18,17 @@ public class MainModel implements DataRequestable {
 
     private FirebaseFirestore db;
     private List<String> subscription_list;
-    final CollectionReference collection_reference;
-    private String user_name;
+    private User current_user;
+    private Experiment chosen_experiment;
 
     private MainModel(){
         db = FirebaseFirestore.getInstance();
-        collection_reference = db.collection("Subscribed");
+
         /**
-         * HAS TO BE REFACTORED WHEN USER CLASS IS READY
+         * HAS TO BE REFACTORED AFTER FIREBASE DATA QUERY IS READY
          */
         // TODO implement after USER is done
-        user_name = "User0000";
+        current_user = new User("User0000", "John", "john@mail.com", "780 999 9999");
     }
 
     /**
@@ -62,9 +64,94 @@ public class MainModel implements DataRequestable {
     }
 
 
-
     public List<String> requestMyExpIndex() {
         CollectionReference ref = db.collection("Users").document("User0000").collection("Subscriptions");
         return new ArrayList<String>();
+    }
+
+    /**
+     * Setting the chosen experiment and store in this global space
+     *
+     * @throws Exception thrown when either the MainModel is not instantiated
+     */
+    public static void setCurrentExperiment(Experiment experiment) throws Exception {
+        if (single_instance == null) {
+            throw new Exception("single_instance is not initiated");
+        }
+        single_instance.chosen_experiment = experiment;
+    }
+
+    /**
+     * Remove the chosen experiment and store in this global space. THIS RENDERS CHOSEN_EXPERIMENT TO NULL!
+     *
+     * @throws Exception thrown when either the MainModel is not instantiated
+     */
+    public static void removeChosenExperiment() throws Exception {
+        if (single_instance == null) {
+            throw new Exception("single_instance is not initiated");
+        }
+        single_instance.chosen_experiment = null;
+    }
+
+    /**
+     * Getting the chosen experiment stored in this global space
+     *
+     * @return chosen {@link Experiment}
+     * @throws Exception {@link NullPointerException} thrown when either the MainModel is not instantiated or no
+     * experiment was chosen
+     */
+    public static Experiment getCurrentExperiment() throws Exception {
+        if (single_instance == null) {
+            throw new Exception("single_instance is not initiated");
+        }
+
+        if (single_instance.chosen_experiment == null) {
+            throw new Exception("No experiment was chosen (NullPointerException)");
+        }
+
+        return single_instance.chosen_experiment;
+    }
+
+    /**
+     * Setting the user and store in this global space
+     *
+     * @throws Exception thrown when either the MainModel is not instantiated
+     */
+    public static void setCurrentUser(User user) throws Exception {
+        if (single_instance == null) {
+            throw new Exception("single_instance is not initiated");
+        }
+        single_instance.current_user = user;
+    }
+
+    /**
+     * Remove the chosen user and store in this global space. THIS RENDERS CURRENT_USER TO NULL!
+     *
+     * @throws Exception thrown when either the MainModel is not instantiated
+     */
+    public static void removeCurrentUser() throws Exception {
+        if (single_instance == null) {
+            throw new Exception("single_instance is not initiated");
+        }
+        single_instance.chosen_experiment = null;
+    }
+
+    /**
+     * Getting the chosen experiment stored in this global space
+     *
+     * @return chosen {@link Experiment}
+     * @throws Exception {@link NullPointerException} thrown when either the MainModel is not instantiated or no
+     * experiment was chosen
+     */
+    public static User getCurrentUser() throws Exception {
+        if (single_instance == null) {
+            throw new Exception("single_instance is not initiated");
+        }
+
+        if (single_instance.chosen_experiment == null) {
+            throw new Exception("No User was set (NullPointerException)");
+        }
+
+        return single_instance.current_user;
     }
 }
