e299758c151eecd7cd61a65666d962a10013eaae
Danh Nguyen <65102457+danny-cpp@users.noreply.github.com>
2021-03-16 16:17:40 -0400

    Revert "Clean up and enforce private"
    This reverts commit 02383bcda761f1d16b729e6743dbc56db9d3fee0.

--- app/src/main/java/com/example/appraisal/UI/MainActivity.java
+++ app/src/main/java/com/example/appraisal/UI/MainActivity.java
@@ -10,8 +10,9 @@ import com.example.appraisal.R;
 import com.example.appraisal.model.MainModel;
 
 public class MainActivity extends AppCompatActivity {
-    
-    private String id;
+
+
+    String id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,4 +28,44 @@ public class MainActivity extends AppCompatActivity {
         Intent begin_intent = new Intent(this, BeginActivity.class);
         startActivity(begin_intent);
     }
+
+
+//    public void toSelection(View v) {
+//        // TODO Bring the user from main menu, to experiment type menu
+//        Intent intent = new Intent(this, SelectionActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void toHome(View v) {
+//        // TODO Bring the user from main menu, to experiment type menu
+//        Intent intent = new Intent(this, ExpSubscriptionActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void toMyExperiments(View v){
+//        // TODO Bring the user from main menu, to experiment type menu
+//        Intent intent = new Intent(this, MyExperimentActivity.class);
+//        startActivity(intent);
+//    }
+
+//    public void quickTest(View v) throws Exception {
+//
+//        DocumentReference user_reference = MainModel.getUserReference();
+//
+//        user_reference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+//
+//                String user_name = value.get("user_name").toString();
+//
+//                Log.d("onEvent: ",user_name);
+//
+//
+//            }
+//        });
+//
+//    }
+
+
+
 }
