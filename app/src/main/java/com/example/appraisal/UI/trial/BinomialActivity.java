7f513000db9b394d606cdc4a34b0acb1d5df86d0
Danh Nguyen <huudanh2908@gmail.com>
2021-03-28 18:16:50 -0400

    Slight update to read qr

--- app/src/main/java/com/example/appraisal/UI/trial/BinomialActivity.java
+++ app/src/main/java/com/example/appraisal/UI/trial/BinomialActivity.java
@@ -179,6 +179,5 @@ public class BinomialActivity extends AppCompatActivity {
             }
         });
 
-
     }
 }
--- app/src/main/java/com/example/appraisal/model/main_menu/specific_experiment_details/QRAnalyzerModel.java
+++ app/src/main/java/com/example/appraisal/model/main_menu/specific_experiment_details/QRAnalyzerModel.java
@@ -9,14 +9,26 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 
 import com.example.appraisal.R;
-import com.example.appraisal.backend.experiment.Experiment;
-import com.example.appraisal.backend.trial.Trial;
+import com.example.appraisal.model.core.MainModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.Result;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * This class parses and display the result from the camera scanner
@@ -26,6 +38,8 @@ public class QRAnalyzerModel {
     private final Activity parent_activity;
     private final ImageView qr_code_image;
     int firebase_num_trials = 0;
+    private String experimenterID;
+    private CollectionReference experiment_reference;
 
     public QRAnalyzerModel(@NonNull Activity parent_activity) {
         this.parent_activity = parent_activity;
@@ -38,7 +52,7 @@ public class QRAnalyzerModel {
      * @param result -- the detection result
      * @throws WriterException -- when MultiFormatWriter refuse to write
      */
-    public void readingQRCode(@NonNull Result result) throws WriterException {
+    public void readingQRCode(@NonNull Result result) throws Exception {
         String qr_display = result.getText();
 
         // Display the raw value of the code to activity
@@ -66,6 +80,8 @@ public class QRAnalyzerModel {
         qr_code_image.setImageBitmap(bitmap);
 
         String[] commands = decodeTrialQR(qr_display);
+
+        modifyExperiment("03zscDmAc6POCAQTlDEJRChFLsh21", commands[0], commands[1]);
     }
 
     /**
@@ -83,7 +99,6 @@ public class QRAnalyzerModel {
     public String[] decodeTrialQR(String encoded_info) {
         String[] commands = encoded_info.split(";");
 
-
         return commands;
     }
 
@@ -93,11 +108,81 @@ public class QRAnalyzerModel {
      * Make the Trial object.
      * After that, add the Trial, and upload.
      *
-     * @param target_qr
-     * @param just_created
+     * @param ID
+     * @param trial_type
+     * @param value
      */
-    public void modifyExperiment(Experiment target_qr, Trial just_created) {
-        // TODO
+    public void modifyExperiment(String ID, String trial_type, String value) throws Exception {
+        CollectionReference ref = MainModel.getExperimentReference();
+
+        Map<String, Object> trial_info = new HashMap<>();
+        String name = "Trial" + firebase_num_trials + 1;
+
+        if (trial_type.equals("binomial")) {
+            if (value.equals("1")) {
+                trial_info.put("result", "1"); // 1 indicates success
+            } else {
+                trial_info.put("result", "0"); // 0 indicates failure
+            }
+        }
+
+
+
+
+        // put trial date as current date
+        DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+        String date_string = formatter.format(Calendar.getInstance().getTime());
+        trial_info.put("date", date_string);
+
+
+        try {
+            experimenterID = MainModel.getCurrentUser().getID();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        trial_info.put("experimenterID", experimenterID);
+
+        // create new document for experiment with values from hash map
+        experiment_reference.document(ID).collection("Trials").document(name).set(trial_info)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("***", "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("***", "Error writing document", e);
+                    }
+                });
+
+        experiment_reference.document(ID).update("numOfTrials", String.valueOf(firebase_num_trials + 1));
+
+        Log.d("herere", "SCAAAAAAAAAAAAAAAAAN!");
+    }
+
+    private void getTrialInt(String exp_id) throws Exception {
+        experiment_reference = MainModel.getExperimentReference();
+
+        experiment_reference.document(exp_id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+
+                    if ((document != null) && document.exists()) {
+                        try {
+                            firebase_num_trials = Integer.parseInt(document.get("numOfTrials").toString());
+                            Log.d("numtrials listener", String.valueOf(firebase_num_trials));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+            }
+        });
     }
 
     public boolean isRegisteredCode(Result result) {
