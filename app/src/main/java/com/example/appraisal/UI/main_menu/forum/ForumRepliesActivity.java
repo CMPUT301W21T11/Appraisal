d35f5c79be1eec08034d9b9a659e3cf8849b23fa
R <mnaveed@ualberta.ca>
2021-03-27 19:15:55 -0600

    forum firebase implementation working

--- app/src/main/java/com/example/appraisal/UI/main_menu/forum/ForumRepliesActivity.java
+++ app/src/main/java/com/example/appraisal/UI/main_menu/forum/ForumRepliesActivity.java
@@ -2,34 +2,48 @@ package com.example.appraisal.UI.main_menu.forum;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.GridLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.appraisal.R;
-import com.example.appraisal.UI.MainActivity;
-import com.example.appraisal.UI.main_menu.MainMenuCommonActivity;
-import com.example.appraisal.UI.main_menu.my_experiment.MyExperimentActivity;
-import com.example.appraisal.UI.main_menu.specific_experiment_details.SpecificExpDiscussionFragment;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.example.appraisal.backend.experiment.Experiment;
+import com.example.appraisal.model.MainModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ForumRepliesActivity extends  AppCompatActivity {
 
-    private ListView reply_list;
-    private ArrayList<String> replies;
+    private ListView reply_display;
+    private ArrayList<String> replies_list;
     private ArrayAdapter<String> reply_adapter;
     private EditText reply_input;
+    private Experiment current_experiment;
+    private CollectionReference exp_ref;
+    private int firebase_num_replies = 0;
+    private String reply_text;
+    private String question_name_receive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,21 +51,34 @@ public class ForumRepliesActivity extends  AppCompatActivity {
         setContentView(R.layout.activity_forum_replies);
 
         String question = getIntent().getStringExtra("Question");
+        question_name_receive = getIntent().getStringExtra("Q_id_name");
 
         TextView textView = findViewById(R.id.question);
         textView.setText(question);
 
-        reply_list = findViewById(R.id.forum_replies);
-        replies = new ArrayList<>();
 
-        String []experiments = {"Reply 1", "Reply 2", "Reply 3", "Reply 4"};
-        Collections.addAll(replies, experiments);
-        reply_adapter = new ArrayAdapter<>(this, R.layout.list_content, replies);
+        try {
+            current_experiment = MainModel.getCurrentExperiment();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        reply_list.setAdapter(reply_adapter);
+        try {
+            exp_ref = MainModel.getExperimentReference();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        //posting a new reply
-        final Button publishNewReply = findViewById(R.id.reply_button);
+        reply_display = findViewById(R.id.forum_replies);
+        replies_list = new ArrayList<>();
+        reply_adapter = new ArrayAdapter<>(this, R.layout.list_content, replies_list);
+
+        getDbReplies();
+
+        reply_display.setAdapter(reply_adapter);
+
+        //dialogue box to publish a new reply
+        final Button publishNewReply = findViewById(R.id.AddReplyButton);
         publishNewReply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,11 +88,25 @@ public class ForumRepliesActivity extends  AppCompatActivity {
                 reply_input = new EditText(ForumRepliesActivity.this);
                 builder.setView(reply_input);
 
+                try {
+                    exp_ref = MainModel.getExperimentReference();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    current_experiment = MainModel.getCurrentExperiment();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                listenToNumOfReplies();
+
                 builder.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        String text = reply_input.getText().toString();
-                        replies.add(text);
+                        reply_text = reply_input.getText().toString();
+                        storeReplyInFireBase();
                         reply_adapter.notifyDataSetChanged();
                     }
                 });
@@ -81,6 +122,83 @@ public class ForumRepliesActivity extends  AppCompatActivity {
                 reply_box.show();
             }
         });
+    }
 
+    /**
+     * Queries the database to get the list of replies questions
+     */
+    private void getDbReplies() {
+        exp_ref.document(current_experiment.getExpId()).collection("Questions").document(question_name_receive).collection("Replies").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                // clear old list
+                replies_list.clear();
+
+                // check each replies document
+                for (QueryDocumentSnapshot doc : value) {
+                    String reply_text = doc.getData().get("reply").toString();
+
+                    // add replies to the list to display
+                    replies_list.add(reply_text);
+
+                }
+                // notify adapter that data has change and to update the UI
+                reply_adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+
+    /**
+     * Queries the database to get the total number of replies for the question
+     */
+    private void listenToNumOfReplies() {
+
+        exp_ref.document(current_experiment.getExpId()).collection("Questions").document(question_name_receive).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+
+                    if ((document != null) && document.exists()) {
+                        try {
+                            firebase_num_replies = Integer.parseInt(document.get("numOfReplies").toString());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * Publish the replies to firebase
+     */
+    public void storeReplyInFireBase() {
+
+        String experiment_ID = current_experiment.getExpId();
+
+        Integer num_of_replies = firebase_num_replies + 1;
+        String reply_name = "Reply" + num_of_replies;
+        Map<String, Object> replies_info = new HashMap<>();
+        replies_info.put("reply", reply_text);
+
+        // create new document for replies with values from hash map
+        exp_ref.document(experiment_ID).collection("Questions").document(question_name_receive).collection("Replies").document(reply_name).set(replies_info)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("***", "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("***", "Error writing document", e);
+                    }
+                });
+
+        exp_ref.document(experiment_ID).collection("Questions").document(question_name_receive).update("numOfReplies", num_of_replies);
     }
 }
--- app/src/main/java/com/example/appraisal/UI/main_menu/specific_experiment_details/SpecificExpDiscussionFragment.java
+++ app/src/main/java/com/example/appraisal/UI/main_menu/specific_experiment_details/SpecificExpDiscussionFragment.java
@@ -3,13 +3,12 @@ package com.example.appraisal.UI.main_menu.specific_experiment_details;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -20,68 +19,109 @@ import androidx.fragment.app.Fragment;
 
 import com.example.appraisal.R;
 import com.example.appraisal.UI.main_menu.forum.ForumRepliesActivity;
-import com.example.appraisal.UI.main_menu.my_experiment.PublishExpActivity;
+import com.example.appraisal.backend.experiment.Experiment;
+import com.example.appraisal.backend.user.User;
+import com.example.appraisal.model.MainModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SpecificExpDiscussionFragment extends Fragment {
 
-    private ListView question_list;
-    private ArrayList<String> questions;
+    private ListView question_display;
+    private ArrayList<String> questions_list;
     private ArrayAdapter<String> question_adapter;
     private EditText question_input;
+    private Experiment current_experiment;
+    private CollectionReference exp_ref;
+    private int firebase_num_questions = 0;
+    private String ques_text;
+    private String question_name_pass;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_specific_exp_discussion, container, false);
 
-        question_list = v.findViewById(R.id.forum_questions);
-        questions = new ArrayList<>();
+        try {
+            current_experiment = MainModel.getCurrentExperiment();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        String[] experiments = {"Question 1", "Question 2", "Question 3"};
-        Collections.addAll(questions, experiments);
+        try {
+            exp_ref = MainModel.getExperimentReference();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        question_adapter = new ArrayAdapter<>(this.getActivity(), R.layout.list_content, questions);
-        question_list.setAdapter(question_adapter);
+        question_display = v.findViewById(R.id.forum_questions);
+        questions_list = new ArrayList<>();
+        question_adapter = new ArrayAdapter<>(this.getActivity(), R.layout.list_content, questions_list);
 
-        //Intent intent = new Intent(this.getContext(), ForumRepliesActivity.class);
-        // question_list.setOnItemClickListener((parent, view, position, id) -> startActivity(intent));
+        getDbQuestions();
 
-        question_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        question_display.setAdapter(question_adapter);
+
+        question_display.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
 
                 int selected_question = position;
-                String question_string = questions.get(selected_question);
+                String question_string = questions_list.get(selected_question);
+                question_name_pass = "Question" + (position+1);
+
 
                 Intent intent = new Intent(getContext(), ForumRepliesActivity.class);
                 intent.putExtra("Question", question_string);
+                intent.putExtra("Q_id_name",question_name_pass);
 
                 startActivity(intent);
             }
         });
 
-
-        //posting a new question
+        //dialogue box to publish a new question
         final FloatingActionButton publishNewQuestion = v.findViewById(R.id.AddQuestionButton);
         publishNewQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder builder  = new AlertDialog.Builder(getContext());
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
                 builder.setTitle("Post a question!");
 
                 question_input = new EditText(getContext());
                 builder.setView(question_input);
 
+                try {
+                    exp_ref = MainModel.getExperimentReference();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    current_experiment = MainModel.getCurrentExperiment();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                listenToNumOfQuestions();
+
                 builder.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        String text = question_input.getText().toString();
-                        // Collections.addAll(questions,text);
-                        questions.add(text);
+                        ques_text = question_input.getText().toString();
+                        storeQuestionInFireBase();
                         question_adapter.notifyDataSetChanged();
                     }
                 });
@@ -101,7 +141,80 @@ public class SpecificExpDiscussionFragment extends Fragment {
         return v;
     }
 
+    /**
+     * Queries the database to get the list of questions
+     */
+    private void getDbQuestions() {
+        exp_ref.document(current_experiment.getExpId()).collection("Questions").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                // clear old list
+                questions_list.clear();
 
+                // check each question document
+                for (QueryDocumentSnapshot doc : value) {
+                    String question_text = doc.getData().get("question").toString();
 
+                    // add question to the list to display
+                    questions_list.add(question_text);
 
+                }
+                // notify adapter that data has change and to update the UI
+                question_adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    /**
+     * Queries the database to get the total number of questions for the experiment
+     */
+    private void listenToNumOfQuestions() {
+
+        exp_ref.document(current_experiment.getExpId()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+
+                    if ((document != null) && document.exists()) {
+                        try {
+                            firebase_num_questions = Integer.parseInt(document.get("numOfQuestions").toString());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * Publish the questions to firebase
+     */
+    public void storeQuestionInFireBase() {
+
+        String experiment_ID = current_experiment.getExpId();
+
+        Integer num_of_questions = firebase_num_questions + 1;
+        String question_name = "Question" + num_of_questions;
+        Map<String, Object> question_info = new HashMap<>();
+        question_info.put("question", ques_text);
+
+        // create new document for question with values from hash map
+        exp_ref.document(experiment_ID).collection("Questions").document(question_name).set(question_info)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("***", "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("***", "Error writing document", e);
+                    }
+                });
+
+        exp_ref.document(experiment_ID).update("numOfQuestions", num_of_questions);
+    }
 }
