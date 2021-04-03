587f7fd7484ab57cc4aedfdcc241d16f558ac95d
flyrobot27 <stevenheung.ca@gmail.com>
2021-04-02 23:39:07 -0300

    Barcode query complete. Still need to complete Scan result

--- app/src/main/java/com/example/appraisal/model/main_menu/specific_experiment_details/BarcodeAnalyzerModel.java
+++ app/src/main/java/com/example/appraisal/model/main_menu/specific_experiment_details/BarcodeAnalyzerModel.java
@@ -2,6 +2,7 @@ package com.example.appraisal.model.main_menu.specific_experiment_details;
 
 import android.app.Activity;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,13 +28,18 @@ import java.util.Map;
 
 public class BarcodeAnalyzerModel extends QRAnalyzerModel{
 
-    private BarcodeScanResult parent_activity;
+    private final BarcodeScanResult parent_activity;
 
     public BarcodeAnalyzerModel(@NonNull BarcodeScanResult parent_activity) {
         super(parent_activity);
         this.parent_activity = parent_activity;
     }
 
+    /**
+     * This method will add the registered barcode to the database
+     * If the barcode is already registered we ask the user if wants to overwrite
+     * @param barcode -- barcode object
+     */
     public void checkBarcode(@NonNull Barcode barcode) {
         String raw_value = barcode.getRawValue();
         try {
@@ -44,28 +50,31 @@ public class BarcodeAnalyzerModel extends QRAnalyzerModel{
                     .document(exp_id)
                     .collection("Barcodes");
 
-            if (barcode_list == null) {
-                addToDatabase(barcode_list, barcode);
-            }
-
             barcode_list.addSnapshotListener((value, error) -> {
                 if (value == null) {
-                    error.printStackTrace();
-                    return;
-                }
-                for (QueryDocumentSnapshot document: value) {
-                    String document_value = document.getId();
-                    String old_value = document.get("action").toString();
-                    if (document_value.equalsIgnoreCase(raw_value)) {
-                        boolean doOverride = parent_activity.askIfOverride(barcode, old_value);
-                        if (doOverride) {
-                            barcode_list.document(raw_value).set(barcode.getData());
-                        } else {
+                    // if either the collection is empty or not yet created
+                    if (error != null) {
+                        Log.d("Barcode Analyzer Model", "Error returned from firebase");
+                        error.printStackTrace();
+                    }
+                    addToDatabase(barcode_list, barcode); // Add the barcode to firebase
+                } else {
+                    // check if barcode is already in the collection
+                    for (QueryDocumentSnapshot document : value) {
+                        String document_value = document.getId();
+                        String old_value = document.get("action").toString();
+                        if (document_value.equalsIgnoreCase(raw_value)) { // if the barcode is found
+                            boolean doOverride = parent_activity.askIfOverride(barcode, old_value); // ask user if they want to override
+                            // if user declined override, return
+                            if (doOverride) {
+                                barcode_list.document(raw_value).set(barcode.getData()); // override data
+                            } else {
+                                Toast.makeText(parent_activity, "Barcode value is NOT overriden", Toast.LENGTH_SHORT).show();
+                            }
                             return;
                         }
-                    } else {
-                        addToDatabase(barcode_list, barcode);
                     }
+                    addToDatabase(barcode_list, barcode); // Add the barcode to firebase
                 }
             });
         } catch (Exception e) {
@@ -77,9 +86,6 @@ public class BarcodeAnalyzerModel extends QRAnalyzerModel{
         Map<String, String> barcode_data = new HashMap<>();
         barcode_data.put("action", barcode.getData());
         barcode_list.add(barcode_data);
-    }
-
-    private void createBarcodeList(DocumentReference user) {
-
+        Toast.makeText(parent_activity, "Successfully set barcode action!", Toast.LENGTH_SHORT).show();
     }
 }
