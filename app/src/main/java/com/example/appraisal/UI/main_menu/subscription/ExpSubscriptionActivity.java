a317e8d2ea5e14e5b000e3626177d7ab2e6d5bd8
Danh Nguyen <huudanh2908@gmail.com>
2021-04-04 14:50:07 -0400

    Distinct

--- app/src/main/java/com/example/appraisal/UI/main_menu/subscription/ExpSubscriptionActivity.java
+++ app/src/main/java/com/example/appraisal/UI/main_menu/subscription/ExpSubscriptionActivity.java
@@ -32,6 +32,7 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class ExpSubscriptionActivity extends MainMenuCommonActivity implements ExpStatusFragment.OnFragmentInteractionListener {
 
@@ -196,6 +197,8 @@ public class ExpSubscriptionActivity extends MainMenuCommonActivity implements E
                                                 subscribed_experiments.add(experiment);
 
                                                 Log.d("Subscribed Experiments:", subscribed_experiments.toString());
+
+                                                subscribed_experiments = (ArrayList<Experiment>) subscribed_experiments.stream().distinct().collect(Collectors.toList());
 
                                                 adapter.notifyDataSetChanged();
 
