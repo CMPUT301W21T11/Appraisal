31714993c3a41cb62530afc4a514577fb8c2db6f
Urvi Patel <upatel@ualberta.ca>
2021-04-08 16:07:28 -0600

    fixed qr resutl display and binomial result display

--- app/src/main/java/com/example/appraisal/UI/main_menu/specific_experiment_details/details/trial_list/ViewTrialAdapter.java
+++ app/src/main/java/com/example/appraisal/UI/main_menu/specific_experiment_details/details/trial_list/ViewTrialAdapter.java
@@ -8,7 +8,10 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.example.appraisal.R;
+import com.example.appraisal.backend.experiment.Experiment;
 import com.example.appraisal.backend.specific_experiment.ViewTrial;
+import com.example.appraisal.backend.trial.TrialType;
+import com.example.appraisal.model.core.MainModel;
 
 import java.util.ArrayList;
 
@@ -19,6 +22,7 @@ import java.util.ArrayList;
 public class ViewTrialAdapter extends ArrayAdapter<ViewTrial> {
     private ArrayList<ViewTrial> trials;
     private Context context;
+    private  Experiment experiment;
 
     public ViewTrialAdapter(Context context, ArrayList<ViewTrial> trials) {
         super(context, 0, trials);
@@ -44,11 +48,32 @@ public class ViewTrialAdapter extends ArrayAdapter<ViewTrial> {
         ViewTrial viewTrial = trials.get(position);
 
         TextView trial_ID = view.findViewById(R.id.trial_ID);
+        // TODO: display success and failure
         TextView result = view.findViewById(R.id.trial_result);
         TextView date = view.findViewById(R.id.view_trial_date);
 
         trial_ID.setText(viewTrial.getID());
-        result.setText(viewTrial.getOutcome());
+
+        try {
+           experiment = MainModel.getCurrentExperiment();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        if (experiment.getType().equals(TrialType.BINOMIAL_TRIAL.getLabel())) {
+            if (Double.parseDouble(viewTrial.getOutcome()) == 0.0) {
+                result.setText("Failure");
+            }
+            else {
+                result.setText("Success");
+            }
+        }
+        else {
+            result.setText(viewTrial.getOutcome());
+        }
+
+
         date.setText(viewTrial.getDate());
 
         return view;
