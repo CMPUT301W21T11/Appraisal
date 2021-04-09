a41b859d88ab5d0dd9f5483767e4f7a789f0a056
flyrobot27 <stevenheung.ca@gmail.com>
2021-04-09 12:17:19 -0300

    add check for if the experiment is ended or unpublished

--- app/src/main/java/com/example/appraisal/UI/main_menu/specific_experiment_details/qr_scanner/CameraScanResult.java
+++ app/src/main/java/com/example/appraisal/UI/main_menu/specific_experiment_details/qr_scanner/CameraScanResult.java
@@ -2,6 +2,7 @@ package com.example.appraisal.UI.main_menu.specific_experiment_details.qr_scanne
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -22,6 +23,8 @@ import com.example.appraisal.backend.specific_experiment.QRValues;
 import com.example.appraisal.backend.trial.TrialType;
 import com.example.appraisal.model.core.MainModel;
 import com.example.appraisal.model.main_menu.specific_experiment_details.QRAnalyzerModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -112,7 +115,6 @@ public class CameraScanResult extends AppCompatActivity {
         model.displayBarCode(result);
 
         final TextView activity_title = findViewById(R.id.camera_scan_result_title);
-//        final TextView experiment_id_display = findViewById(R.id.camera_scan_result_exp_id_display);
         final TextView experiment_desc_display = findViewById(R.id.camera_scan_result_exp_desc_display);
         final TextView trialType = findViewById(R.id.camera_scan_result_trial_type_display);
         final TextView trialValue = findViewById(R.id.camera_scan_result_trial_value_display);
@@ -139,9 +141,8 @@ public class CameraScanResult extends AppCompatActivity {
                     default:
                         trialValue.setText(String.valueOf(values.getValue()));
                 }
-//                experiment_id_display.setText(values.getExpId());
 
-                addGeolocation(values.getExpId());
+                checkIfClosed(values.getExpId());
                 setExperimentDesc(experiment_desc_display, values.getExpId());
 
                 CollectionReference experiment = MainModel.getExperimentReference();
@@ -236,9 +237,8 @@ public class CameraScanResult extends AppCompatActivity {
                         String exp_id = getField(document, "targetExperimentId");
                         String exp_desc = getField(document, "targetExperimentDesc");
 
-                        addGeolocation(exp_id);
+                        checkIfClosed(exp_id);
 
-//                        experiment_id_display.setText(exp_id);
                         experiment_desc_display.setText(exp_desc);
                         trialType.setText(trial_type);
                         trialValue.setText(data);
@@ -285,6 +285,44 @@ public class CameraScanResult extends AppCompatActivity {
                     finish();
                 });
             }
+        }
+    }
+
+    private void checkIfClosed(String exp_id) {
+        try {
+            CollectionReference exp_list = MainModel.getExperimentReference();
+            exp_list.document(exp_id).get().addOnCompleteListener(task -> {
+                if(task.isSuccessful()) {
+                    DocumentSnapshot experiment = task.getResult();
+                    Boolean is_closed = experiment.getBoolean("isEnded");
+                    Boolean is_published = experiment.getBoolean("isPublished");
+                    if ((is_closed != null && is_closed) || (is_published != null && !is_published)) {
+                        AlertDialog alertDialog = new AlertDialog.Builder(self, R.style.AlertDialogTheme)
+                                .setCancelable(false)
+                                .setMessage("Sorry, the target experiment you scanned is closed or unpublished")
+                                .setPositiveButton("Exit", (dialog, which) -> {
+                                    try {
+                                        MainModel.setBarcodeResult(null);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    self.finish();
+                                })
+                                .create();
+                        alertDialog.show();
+                        alertDialog.getButton(alertDialog.BUTTON_POSITIVE).setTextColor(Color.WHITE);
+                    } else { // proceed to next step (add geolocation)
+                        addGeolocation(exp_id);
+                    }
+                } else { // ignore this step and proceed
+                   if (task.getException() != null) {
+                       task.getException().printStackTrace();
+                   }
+                   addGeolocation(exp_id);
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
